90ed502ce5585d8a4f8a53ccf76106a251ded3a5
汪健业 <1067979023@qq.com>
2020-01-07 20:51:00 +0800

    finish

--- play-travel/src/pages/Destination.js
+++ play-travel/src/pages/Destination.js
@@ -24,6 +24,11 @@ class Destination extends Component {
         this.changeType = this.changeType.bind(this);
         this.changeDip = this.changeDip.bind(this);
         this.Search = this.Search.bind(this);
+        this.getsearch = this.getsearch.bind(this);
+        this.setsearch = this.setsearch.bind(this);
+        this.stop = this.stop.bind(this);
+        this.lift = this.lift.bind(this);
+        this.toList = this.toList.bind(this);
     }
     state = {
         currentIdx: 0, //高亮索引值
@@ -32,6 +37,10 @@ class Destination extends Component {
         menu: [],
         hots: [],
         citysearch:[]
+    }
+
+    toList(activeKey) {
+        this.props.history.push('/list/' + activeKey)
     }
 
     async componentDidMount() {
@@ -52,6 +61,10 @@ class Destination extends Component {
         this.changecity(hots);
     }
 
+    stop(e) {
+        e.stopPropagation();
+    }
+
     changecity(hots) {
         this.setState({
             hots
@@ -70,36 +83,82 @@ class Destination extends Component {
         this.changecity(hots);
     }
 
-    changeDip() {
+    changeDip(value) {
         let { dip } = this.state;
+        let val = value.target.value;
+        this.getsearch(val);
         this.setState({
-            dip: !dip
+            dip:!dip
         })
-       
     }
 
-    async Search(value) {
-        console.log(666);
-        let {
-            data
-        } = await My.get('/area/search', {
-            title: value.target.value
-        });
-        console.log(data);
+    lift() {
+        this.setState({
+            dip:1
+        })
+    }
 
+    async getsearch(val) {
+        if (val) {
+            let {
+                data
+            } = await My.get('/area/search', {
+                title: val
+            });
+            this.setState({
+                citysearch: data ? data : []
+            })
+        } else {
+             let data = JSON.parse(localStorage.getItem("searchname"));
+            //  console.log(data);
+             this.setState({
+                 citysearch: data ? data : []
+             })
+        }
+    }
+
+    Search(value) {
+        let val = value.target.value;
+        this.getsearch(val);
+    }
+
+    setsearch(obj) {
+        // console.log(obj);
+        if (obj.title) {
+            let isok = true;
+            let data = JSON.parse(localStorage.getItem("searchname"));
+            console.log(data);
+            data.forEach(item => {
+                if (item.title === obj.title) {
+                    isok = false;
+                }
+            });
+            if (isok) {
+                let searchobj = {
+                    name: obj.title,
+                    code: obj.code,
+                    type:'city'
+                }
+                data.push(searchobj);
+                console.log(data);
+                let str = JSON.stringify(data);
+                localStorage.setItem("searchname", str);
+            }
+        }
+        this.toList(obj.code)
     }
 
     render() {
         let {
             match
         } = this.props;
-        let { citysearch } = this.state.menu;
-        return <div id = "destination" >
+        let { citysearch } = this.state;
+        return <div id = "destination" onClick={this.lift} >
             <header>
                 <h2>下一次您想去哪里玩？</h2>
                 <Input
                     onFocus={this.changeDip}
-                    onBlur={this.changeDip}
+                    onClick={this.stop}
                     onChange={this.Search.bind(this)}
                     placeholder="输入一个目的地"
                     prefix={<Icon type="environment" style={{ color: 'rgba(240,47,69,1)' }} />}
@@ -108,9 +167,17 @@ class Destination extends Component {
                         </Tooltip>
                     }
                 />
-                <ul className={this.state.dip?"dip":''}><li ></li>
-                   
-                    </ul>
+                {
+                    citysearch.length?
+                    <ul className={this.state.dip ? "dip" : ''}>
+                            {citysearch.map((item, idx) => {
+                                return <li
+                                    onClick={this.setsearch.bind(this,{title:item.title,code:item.code})}
+                                    key={item.code}>{item.title ? item.title : item.name}</li>
+                        })}                   
+                    </ul>:''
+                }
+                
                         
                
             </header>
@@ -134,7 +201,8 @@ class Destination extends Component {
                     <div className="top">
                         {
                             this.state.hots.map((item, index) => {
-                                return <p className="hots" style={{backgroundImage:`url(${item.image_url})`}} key={item.code}>
+                                return <p className="hots" style={{ backgroundImage: `url(${item.image_url})` }} key={item.code}
+                                onClick={this.toList.bind(this,item.code)}>
                                     <span>{item.title}</span>
                                     <span>{item.code}</span>
                                 </p>
