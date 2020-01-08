9e168cf532eeec7f17f8b6454fe38f1cedcd7169
Damon <747724100@qq.com>
2020-01-08 09:24:08 +0800

    1.08

--- play-travel/src/pages/Order.js
+++ play-travel/src/pages/Order.js
@@ -6,6 +6,7 @@ class Order extends Component {
     // console.log(this)
     state = {
         currentIdx: 0,
+        data: [],
         deletebutton: false,
         navlist: ['全部订单', '待付款', '处理中', '已确认', '已退订'],
         datalist: [
@@ -49,8 +50,21 @@ class Order extends Component {
     }
 
     changeNav = (idx) => {
+        let datalist = this.state.datalist
+        if (idx === 0) {
+            this.setState({
+                data: datalist
+            })
+        } else if (idx === 1) {
+            this.setState({
+                data: datalist.filter(item => item.status_id === '1')
+            })
+        }
+
+
         this.setState({
             currentIdx: idx,
+
         })
     }
     deleteItem = (id) => {
@@ -77,7 +91,7 @@ class Order extends Component {
 
 
     render() {
-        let { navlist, currentIdx, datalist } = this.state
+        let { navlist, currentIdx, datalist, data } = this.state
         return < div className="order_list" >
             <div className="order_nav">
                 <h2>我的订单</h2>
@@ -88,72 +102,41 @@ class Order extends Component {
                 </ul>
             </div>
             <div className="order_item">
-                {/* {console.log(currentIdx)} */}
-                {datalist.map((item) => {
-                    if (currentIdx === 0) {
-                        return <div
-                            className={item.status_id === '25' ? "single_item_overdue single_item" : "single_item"}
-                            key={item.order_id}
-                        // onClick={this.getDetail.bind(this, item.order_id)}
-                        >
-                            <div className="item_top">
-                                <p className="clearfix first_line ">
-                                    <span className="item_status">{item.status_name}</span>
-                                    <span className="item_price">￥{item.total}</span>
-                                </p>
-                                <p className="second_line clearfix">
-                                    <span className='order_num fl'>订单号:{item.order_id}</span>
-                                    <span className="pay_limit fr">请在{item.payment_time_limit.slice(10, 16)}前支付</span>
-                                </p>
-                            </div>
-                            <div className="item_center">
-                                <p className="item_title">{item.product_name}</p>
-                                <p className="item_time">出行日期:{item.tour_date}</p>
-                                <p className="item_mount">购买数量:套餐份数×1</p>
-
-                            </div>
-                            <div className="item_footer">
-                                <span className="cancel_order">取消订单</span>
-                                <span className="to_pay">去支付</span>
-                                <p className="item_delete" onClick={this.deleteItem}><span className='iconfont icon-shanchu' ></span>删除订单</p>
-                            </div>
-                            <div className="deletepopup" style={{ display: this.state.deletebutton ? "block" : 'none' }}>
-                                <div className="delete_mes">
-                                    <h5 className="delete_title">删除订单</h5>
-                                    <p className="delete_content">确定删除订单吗？删除后订单将不可恢复</p>
-                                    <div className='delete_button'><span className="canceldelete" onClick={this.cancelDelete}>取消</span><span className="confirmdelete" onClick={this.confirmDelete.bind(this, item.order_id)}>删除</span></div>
-                                </div>
-                            </div>
+                {data.map((item) => {
+                    return <div
+                        className={item.status_id === '25' ? "single_item_overdue single_item" : "single_item"}
+                        key={item.order_id}
+                    // onClick={this.getDetail.bind(this, item.order_id)}
+                    >
+                        <div className="item_top">
+                            <p className="clearfix first_line ">
+                                <span className="item_status">{item.status_name}</span>
+                                <span className="item_price">￥{item.total}</span>
+                            </p>
+                            <p className="second_line clearfix">
+                                <span className='order_num fl'>订单号:{item.order_id}</span>
+                                <span className="pay_limit fr">请在{item.payment_time_limit.slice(10, 16)}前支付</span>
+                            </p>
+                        </div>
+                        <div className="item_center">
+                            <p className="item_title">{item.product_name}</p>
+                            <p className="item_time">出行日期:{item.tour_date}</p>
+                            <p className="item_mount">购买数量:套餐份数×1</p>
 
                         </div>
-                    }
-                    else if (currentIdx === 1) {
-                        if (item.status_id === '1') {
-                            return <div className={item.status_id === '25' ? "single_item_overdue single_item" : "single_item"} key={item.order_id}>
-                                <div className="item_top">
-                                    <p className="clearfix first_line ">
-                                        <span className="item_status">{item.status_name}</span>
-                                        <span className="item_price">￥{item.total}</span>
-                                    </p>
-                                    <p className="second_line clearfix">
-                                        <span className='order_num fl'>订单号:{item.order_id}</span>
-                                        <span className="pay_limit fr">请在{item.payment_time_limit.slice(10, 16)}前支付</span>
-                                    </p>
-                                </div>
-                                <div className="item_center">
-                                    <p className="item_title">{item.product_name}</p>
-                                    <p className="item_time">出行日期:{item.tour_date}</p>
-                                    <p className="item_mount">购买数量:套餐份数×1</p>
-                                    <p></p>
-                                </div>
-                                <div className="item_footer">
-                                    <span className="cancel_order">取消订单</span>
-                                    <span className="to_pay">去支付</span>
-                                    <p className="item_delete" onClick={this.deleteItem.bind(this, item.order_id)}><span className='iconfont icon-shanchu'></span>删除订单</p>
-                                </div>
+                        <div className="item_footer">
+                            <span className="cancel_order">取消订单</span>
+                            <span className="to_pay">去支付</span>
+                            <p className="item_delete" onClick={this.deleteItem}><span className='iconfont icon-shanchu' ></span>删除订单</p>
+                        </div>
+                        <div className="deletepopup" style={{ display: this.state.deletebutton ? "block" : 'none' }}>
+                            <div className="delete_mes">
+                                <h5 className="delete_title">删除订单</h5>
+                                <p className="delete_content">确定删除订单吗？删除后订单将不可恢复</p>
+                                <div className='delete_button'><span className="canceldelete" onClick={this.cancelDelete}>取消</span><span className="confirmdelete" onClick={this.confirmDelete.bind(this, item.order_id)}>删除</span></div>
                             </div>
-                        }
-                    }
+                        </div>
+                    </div>
                 })}
             </div>
         </div >
