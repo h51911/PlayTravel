6c497ae9266bb637edd53fd4d9e4e9a074db488d
Yangshennb <1013139205@qq.com>
2020-01-11 10:59:22 +0800

    0111，后台增加旅游项目完成

--- playtravel-bg/src/pages/addgoods.js
+++ playtravel-bg/src/pages/addgoods.js
@@ -1,12 +1,110 @@
 import React from 'react';
 import { Form, Input, DatePicker, Button } from 'antd';
-
+import myweb from '../api/myweb';
 
 class Addgood extends React.Component {
     state = {
-        dataSource: [],
+      product_id:"",
+      cn_name:"",
+      alias:"",
+      min_price:"",
+      from_date:"",
+      to_date:"",
+      priceError:true,
+      start:"",
+      end:""
     };
 
+    id=(e)=>{
+      this.setState({
+        product_id:e.target.value
+      })
+    }
+    city=(e)=>{
+      this.setState({
+        cn_name:e.target.value
+      })
+    }
+    play=(e)=>{
+      this.setState({
+        alias:e.target.value
+      })
+    }
+    price=(e)=>{
+      let reg = new RegExp(/^([1-9][0-9]*)+(.[0-9]{1,2})?$/)
+      if(reg.test(e.target.value)){
+        this.setState({
+          min_price:e.target.value,
+          priceError:true
+        })
+      }else{
+        this.setState({
+          min_price:"",
+          priceError:false
+        })
+      }
+      
+    }
+    startDate=(date,datestring)=>{
+      this.setState({
+        from_date:datestring,
+        start:date['_d']
+      })
+    }
+    endDate=(date,datestring)=>{
+      this.setState({
+        to_date:datestring,
+        end:date['_d']
+      })
+    }
+
+    submit=()=>{
+      let {product_id,cn_name,alias,min_price,from_date,to_date,priceError,start,end} = this.state;
+      if(product_id && cn_name && alias && min_price && from_date && to_date){
+        if(priceError){
+          if(end-start){
+            let result = myweb.get('/list/addPro',{
+              product_id,
+              cn_name,
+              alias,
+              min_price,
+              from_date,
+              to_date
+            })
+            if(result){
+              this.setState({
+                product_id:"",
+                cn_name:"",
+                alias:"",
+                min_price:"",
+                from_date:"",
+                to_date:""
+              })
+            }else{
+              alert('提交错误，请重新提交');
+            }
+          }else{
+            alert('结束时间应在开始时间以后，请重新确认并提交');
+          }
+        }else{
+          alert('价格格式输入不正确，请输入非零开头的最多带两位小数的数字');
+        }
+      }else{
+        alert('请将信息填写完整');
+      }
+    }
+
+    reset=()=>{
+      this.setState({
+        product_id:"",
+        cn_name:"",
+        alias:"",
+        min_price:"",
+        from_date:"",
+        to_date:""
+      })
+    }
+    
     render() {
         return (
             <div id="addgood">
@@ -18,7 +116,13 @@ class Addgood extends React.Component {
                     // validateStatus="error"
                     // help="Should be combination of numbers & alphabets"
                     >
-                        <Input placeholder="输入项目id" id="error1" style={{ width: '500px' }} />
+                        <Input 
+                        placeholder="输入项目id" 
+                        id="error1" 
+                        style={{ width: '500px' }} 
+                        value={this.state.product_id}
+                        onChange={e=>this.id(e)}
+                        />
                     </Form.Item>
                     <Form.Item
                         style={{ display: 'flex' }}
@@ -29,6 +133,7 @@ class Addgood extends React.Component {
                         < Input placeholder="输入游玩城市"
                             id="error2"
                             style={{ width: '500px' }}
+                            onChange={e=>this.city(e)}
                         />
                     </Form.Item>
                     <Form.Item
@@ -40,6 +145,7 @@ class Addgood extends React.Component {
                         < Input placeholder="输入游玩项目"
                             id="error3"
                             style={{ width: '500px' }}
+                            onChange={e=>this.play(e)}
                         />
                     </Form.Item>
                     <Form.Item
@@ -51,6 +157,7 @@ class Addgood extends React.Component {
                         < Input placeholder="输入项目价格"
                             id="error4"
                             style={{ width: '500px' }}
+                            onChange={e=>this.price(e)}
                         />
                     </Form.Item>
                     <Form.Item label="日期" style={{ marginBottom: 0, display: 'flex' }}>
@@ -59,17 +166,17 @@ class Addgood extends React.Component {
                             // help="Please select the correct date"
                             style={{ display: 'inline-block', width: 'calc(50% - 12px)' }}
                         >
-                            <DatePicker />
+                            <DatePicker onChange={this.startDate} />
                         </Form.Item>
                         <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}>-</span>
                         <Form.Item style={{ display: 'inline-block', width: 'calc(50% - 12px)' }}>
-                            <DatePicker />
+                            <DatePicker onChange={this.endDate} />
                         </Form.Item>
                     </Form.Item>
-                    <Button type="primary" block style={{ width: '250px' }}>
+                    <Button type="primary" block style={{ width: '250px' }} onClick={this.submit}>
                         添加项目
                 </Button>
-                    <Button type="danger" block style={{ width: '250px', marginLeft: '20px' }}>
+                    <Button type="danger" onClick={this.reset} block style={{ width: '250px', marginLeft: '20px' }}>
                         重置
                 </Button>
                 </Form>
--- server/backend/router/list.js
+++ server/backend/router/list.js
@@ -7,7 +7,7 @@ const Router = express.Router();
 let { mongo } = require('../db');//引入操作数据库的模块
 
 
-//查询所有用户 /users进入这里
+//查询
 Router.get('/', async (req, res) => {
     let result = await mongo.find('lists', req.query);//调用封装好的find方法，查询数据并返回给前端 [{},{},{}]
     // console.log( req.query);
@@ -33,6 +33,48 @@ Router.get('/addGood',async (req,res)=>{
         res.send(formatData({ code: 0 }));
     }
 });
+
+//新增旅游项目
+Router.get('/addPro',(req,res)=>{
+    // let {product_id,cn_name,alias,min_price,from_date,to_date} = req.query;
+    res.send(88)
+    // let digi = {};
+    // digi.product_id = product_id;
+    // digi.alias = alias;
+    // digi.min_price = min_price;
+    // digi.from_date = from_date;
+    // digi.to_date = to_date;
+    // let res1 = await mongo.find('lists',cn_name);
+    // if(res1.length){
+    //     let {products}= res1;
+    //     if(Array.isArray(products)){
+    //         products.push(digi);
+    //     }else{
+    //         let products=[];
+    //         products.push(digi);
+    //     }
+        
+    //     let res3 = await mongo.update('lists',{cn_name},{$set:{products}})
+    //     if(res3.modifiedCount){
+    //       //成功
+    //       res.send(formatData());
+    //     } else {
+    //         //失败
+    //         res.send(formatData({ code: 0 }));
+    //     }
+    // }else{
+    //     let digiarr = [];
+    //     digiarr.push(digi);
+    //     let res2 = await mongo.create('lists',[cn_name,digiarr])
+    //     if(res2.insertedCount){
+    //         //成功
+    //         res.send(formatData());
+    //     } else {
+    //         //失败
+    //         res.send(formatData({ code: 0 }));
+    //     }
+    // }
+})
 
 
 
